ce45ad4a227ec35c4b079de89a1a7b0dee6b24a5
sionc <sion.chaudhuri@gmail.com>
2013-06-01 15:05:09 -0700

    [Setup] First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sionc::Application.config.secret_key_base = 'a1240bd0fa1eebc4d6a0b0042e37e3510eb490e5eed1321ee13f0b41b1b348bb80dcc681c3952dd725ae73f43baf89c927697d95bbe7f85bdd0d543057f2c0e1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sionc::Application.config.session_store :cookie_store, key: '_sionc_session'
